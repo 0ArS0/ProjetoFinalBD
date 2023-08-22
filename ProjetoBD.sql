35c95de5d77d58b726c33a71524e82280c619879
CarlosAlbertoMPZ <carlosalbertompz@gmail.com>
2023-08-21 22:19:58 -0300

    Projeto parte Carlos Paz

--- ProjetoBD.sql
+++ ProjetoBD.sql
@@ -33,6 +33,23 @@ nome text,
 descricao text
 );
 
+-- Carlos Paz
+create table pedido(
+id integer primary key autoincrement not null,
+id_produto integer,
+id_usuario_comprador integer,
+data_pedido date,
+foreign key (id_produto)references produto(id),
+foreign key (id_usuario_comprador)references usuario(id)
 
+);
+
+create table pedido_produto(
+id integer primary key autoincrement not null,
+id_produtos integer,
+id_pedidos integer,
+foreign key (id_produtos)references produto(id),
+foreign key (id_pedidos)references pedido(id)
+);
 
 
